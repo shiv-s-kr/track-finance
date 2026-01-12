1be099f2e592b00e4815b04f35ac0a5dde9f475a
Shiv Sagar Kumar <ssk835210@gmail.com>
2026-01-12 14:27:20 +0530

    changes db function

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,18 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
    host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+    waitForConnections: true,
+    enableKeepAlive: true,
 });
 
-db.connect(err => {
-    if (err) throw err;
-    console.log('MySQL Connected');
-});
+// db.connect(err => {
+//     if (err) throw err;
+//     console.log('MySQL Connected');
+// });
 
 module.exports = {db};
